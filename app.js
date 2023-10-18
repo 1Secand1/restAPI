955f707634385fc2b55b84fd3a7baa3f515d89c5
1Secand1 <boevladik@gmail.com>
2023-10-18 19:14:38 +0300

    test

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+let contacts = [
+  {
+    id: 1,
+    name: "Влад",
+    value: "число 42",
+    marked: false,
+  },
+];
+
 app.use(express.static(path.relative(__dirname, "client")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.relative(__dirname, "client", "index.html"));
 });
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log(`Server has been started on port 3000...`);
 });
--- client/script.js
+++ client/script.js
@@ -17,8 +17,27 @@ const Counter = {
       this.form.value = this.form.name = "";
     },
 
-    markContact() {},
+    markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      contact.marked = true;
+    },
+    removeContact(id) {
+      this.contacts = this.contacts.filter((c) => c.id !== id);
+    },
   },
 };
+
+function request(url, method = "GET") {
+  try {
+    const headers = "";
+
+    fetch(url, {
+      method,
+    });
+    
+  } catch (e) {
+    console.warn("Error", e.message);
+  }
+}
 
 Vue.createApp(Counter).mount("#app");
